8626821a1887b0b206fad174259ee4ce41ee8432
ritika2004 <ritikaagrawal079@gmail.com>
2026-06-05 18:27:39 +0530

    Docker file

---
+++ src/test/java/Runner/TestRunner.java
@@ -0,0 +1,17 @@
+package Runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"StepDefinitions"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports.html"
+        },
+        monochrome = true
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+}
